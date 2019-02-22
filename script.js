fbb74f305a79053189e723f61995782a3e33177e
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-22 11:38:25 +0100

    counter does not work

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-"use strict"
+"use strict";
 
 var Counter = React.createClass({
-    getInitialState: function () {
-        return {
-            counter: 0
-        };
-    },
+  getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
 
-    increment: function () {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-    decrement: function () {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
+  increment: function() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+  decrement: function() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
 
-    render: function () {
-        return React.createElement('div', {
-                onClick: this.increment,
-            },
-            React.createElement('button', {}, 'Licznik ', +this.state.counter),
-            React.createElement('button', {}, 'Licznik ', +this.state.counter),
-            React.createElement('div', {}, +this.state.counter)
-        );
-    }
+  render: function() {
+    return React.createElement(
+      "div",
+      {
+        onClick: this.increment,
+        onClick: this.decrement
+      },
+      React.createElement("button", {}, "Licznik ", this.increment),
+      React.createElement("button", {}, "Licznik ", this.decrement),
+      React.createElement("div", {}, this.state.counter)
+    );
+  }
+
+  // render: function(){
+  //  return (
+  // <h1>{this.state.counter}</h1>
+  //<button onClick={this.increment}>Increment</button>
+  //<button onClick={this.decrement}>Decrement</button>
+  //      );
 });
 var element = React.createElement(Counter);
-ReactDOM.render(element, document.getElementById('app'));
+ReactDOM.render(element, document.getElementById("app"));
